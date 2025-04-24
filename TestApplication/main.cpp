236f0cacc9aeec33b35519a19c3f64509403c9c5
coderman197208 <gyb47434@gmail.com>
2025-04-24 12:30:36 +0800

    20250424

--- TestApplication/main.cpp
+++ TestApplication/main.cpp
@@ -1,18 +1,22 @@
 ﻿#include <cstdio>
 #include <chrono>
-#include <thread>
+#include <thread>       //std::this_thread::sleep_for   std::chrono::milliseconds
+#include <unistd.h>     //sleep(1)
+#include <signal.h>
 
 #include "..//include//hello.h"
 
 int main()
 {
+	signal(SIGHUP, SIG_IGN);  // 忽略 SIGHUP 信号
 	printf("sizeof(char)=%d\n", sizeof(char));
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 200; i++) {
         printf("%s 向你问好啦!!! %d\n", "TestApplication", i);
         hello();
         hello(i);
         hello2();
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));  // 休眠 100 毫秒
+        //std::this_thread::sleep_for(std::chrono::milliseconds(100));  // 休眠 100 毫秒
+		sleep(1);  // 休眠 1 秒
     }
 	int a = add(88, 9);
     printf("88+9=%d\n", a);

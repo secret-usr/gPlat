76dbab2925ad92dfdedcc9a78151ef31bafae31d
coderman197208 <gyb47434@gmail.com>
2026-02-20 20:26:09 +0800

    260220-01: add project s7ioserver

---
+++ s7ioserver/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <cstdio>
+#include <chrono>
+#include <thread>       //std::this_thread::sleep_for   std::chrono::milliseconds
+#include <atomic>
+#include <unistd.h>     //sleep(1)
+#include <signal.h>
+#include <iostream>
+#include <string.h>
+
+#include "../include/higplat.h"
+
+std::atomic<bool> g_running(true);  // 控制线程运行的标志
+
+void threadFunction1();
+
+int main()
+{
+    // 创建并启动线程
+    std::thread worker1(threadFunction1);
+
+    // 或者分离线程让它独立运行
+    //worker.detach();
+
+    // 主线程可以继续做其他工作...
+    // 主线程等待键盘输入
+    std::string input;
+    while (true) {
+        std::cin >> input;  // 等待用户输入
+        if (input == "q") {
+            g_running = false;  // 通知线程退出
+            std::cout << "exiting..." << std::endl;
+            break;
+        }
+        else {
+            std::cout << "unknown command，input 'q' exit" << std::endl;
+        }
+    }
+
+    // 如果需要等待线程完成（虽然这个线程是无限循环）
+    worker1.join();
+
+    std::cout << "main thread exit" << std::endl;
+
+    return 0;
+}
